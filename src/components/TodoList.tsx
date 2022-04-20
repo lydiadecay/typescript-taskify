0ec0bd681f33c2802ff3958853c2d4a103a7dd11
lydiadecay <lydiadecay@gmail.com>
2022-04-20 19:40:26 +0200

    added functionality, styling

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Todo } from '../model'
+import SingleTodo from './SingleTodo'
 import "./styles.css"
 
 interface Props{
@@ -7,10 +8,17 @@ interface Props{
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
-const TodoList: React.FC = ({todos, setTodos}: Props) => {
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
   return (
     <div className='todos'>
-      
+    {todos?.map((todo) => (
+        <SingleTodo
+        todo={todo}
+        key={todo.id}
+        todos={todos}
+        setTodos={setTodos}/>
+    ))}
+
     </div>
   )
 }
